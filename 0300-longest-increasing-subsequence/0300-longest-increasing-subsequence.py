d054ab10508b982b44ed4e054d33ab5a8daf1236
Seungho Jang <hoosong0235@gmail.com>
2023-03-02 22:04:39 +0900

    Update 0300-longest-increasing-subsequence.py

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         dp = [1 for _ in range(len(nums))]
 
         for i in range(len(nums)):
-            for j in range(i + 1):
+            for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
 

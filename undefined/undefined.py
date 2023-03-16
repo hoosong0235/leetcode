b454ff5d4fc588f86deff4fce003d141abbbcb9f
Seungho Jang <hoosong0235@gmail.com>
2023-03-16 16:29:45 +0900

    Delete undefined directory

--- undefined/undefined.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        vowel = ['a', 'e', 'i', 'o', 'u']
-        return sum(map(lambda x: 1, filter(lambda word: word[0] in vowel and word[-1] in vowel, words[left:right+1])))
